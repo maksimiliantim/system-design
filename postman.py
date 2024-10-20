e218e58f2e9f19bb73c2a2eeafee176f9c04dd09
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2024-10-21 00:28:02 +0300

    Update postman.py

--- postman.py
+++ postman.py
@@ -1,19 +1,46 @@
-### http://127.0.0.1:8080/budget
+
+{
+    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyOTQ1OTM0MCwianRpIjoiODNlMWQ2YmUtZjAzMS00NDBlLTllZmMtNTJhNmI0OWRhNDNkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFkbWluIiwibmJmIjoxNzI5NDU5MzQwLCJjc3JmIjoiYmY2ZjNlNzEtNWMxOC00ZjE0LTgwMDMtMDM0YzQ0OTY3MmQxIiwiZXhwIjoxNzI5NDYwMjQwfQ.PT4okWT1VUJ82IY0XW-y_P-deqBBSTziTYdi3h4TKuQ"
+}
+
+{
+    "username": "admin",
+    "password": "secret"
+}
+{
+    "id": "19b75ab2-a9d3-45a5-8383-09c47cf74731",
+    "username": "admin"
+}
+{
+    "amount": 100.0,
+    "description": "Описание бюджета",
+    "id": "dd72f981-919f-418f-b0a1-ce2c50415405",
+    "user": "admin"
+}
+
 [
+    {
+        "amount": 77.0,
+        "description": "Описание бюджета",
+        "id": "4a28f562-a383-41a6-9037-efb5bc9eca32",
+        "user": "admin"
+    },
     {
         "amount": 100.5,
         "description": "Описание бюджета",
-        "id": "e2112df8-e0be-48a6-82bc-8cff07076e17",
-        "user": "whops"
+        "id": "178bedb4-2fc3-4dad-9d6f-8dfd89b8028b",
+        "user": "admin"
+    },
+    {
+        "amount": 77.0,
+        "description": "Описание бюджета",
+        "id": "feaa27d4-a0a4-4d87-a32d-368b0100e1ed",
+        "user": "admin"
+    },
+    {
+        "amount": 100.0,
+        "description": "Описание бюджета",
+        "id": "dd72f981-919f-418f-b0a1-ce2c50415405",
+        "user": "admin"
     }
 ]
-
-
-### http://127.0.0.1:8080/login
-{
-    "username": "whops",
-    "password": "2007"  
-}
-{
-    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyOTQ1ODQ4MywianRpIjoiNzRmMDMxNmItYjQ4MC00ZDM2LTlhMzEtM2Q5Y2M5ZmU2ZjI4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Indob3BzIiwibmJmIjoxNzI5NDU4NDgzLCJjc3JmIjoiZjEyZjU3M2EtNjM5ZS00ZTI2LWFiZmItNjZhYjc2Njk5NjRmIiwiZXhwIjoxNzI5NDU5MzgzfQ.iVTe4EcbI8QUTgqGRi0IgwVOc-S7Ix5QwhfU0-N72OI"
-}
