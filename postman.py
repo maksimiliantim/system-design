479b2391cc7955cbf51fd1bef75145fea0d55d4c
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2024-10-21 00:09:52 +0300

    Create postman.py

---
+++ postman.py
@@ -0,0 +1,19 @@
+### http://127.0.0.1:8080/budget
+[
+    {
+        "amount": 100.5,
+        "description": "Описание бюджета",
+        "id": "e2112df8-e0be-48a6-82bc-8cff07076e17",
+        "user": "whops"
+    }
+]
+
+
+### http://127.0.0.1:8080/login
+{
+    "username": "whops",
+    "password": "2007"  
+}
+{
+    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcyOTQ1ODQ4MywianRpIjoiNzRmMDMxNmItYjQ4MC00ZDM2LTlhMzEtM2Q5Y2M5ZmU2ZjI4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6Indob3BzIiwibmJmIjoxNzI5NDU4NDgzLCJjc3JmIjoiZjEyZjU3M2EtNjM5ZS00ZTI2LWFiZmItNjZhYjc2Njk5NjRmIiwiZXhwIjoxNzI5NDU5MzgzfQ.iVTe4EcbI8QUTgqGRi0IgwVOc-S7Ix5QwhfU0-N72OI"
+}
